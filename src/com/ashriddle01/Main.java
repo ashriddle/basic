b4220321d09cd26fd67f7858792343c641cefb47
ashriddle <hyjobmedical@163.com>
2020-03-14 06:17:45 +0800

    Initial commit

---
+++ src/com/ashriddle01/Main.java
@@ -0,0 +1,10 @@
+package com.ashriddle01;
+
+public class Main {
+    
+        public static void main (String[] args)
+        {
+            System.out.println("this is a test java class.");
+        }
+
+}
